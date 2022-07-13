43d2142a83a3f8036f62cbf9e64bc38937242d8a
Gauravjattux <108624421+Gauravjattux@users.noreply.github.com>
2022-07-13 14:13:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ async function startGojoMdNx() {
     const callerId = json.content[0].attrs['call-creator']
     if (json.content[0].tag == 'offer') {
     let pa7rick = await GojoMdNx.sendContact(callerId, global.owner)
-    GojoMdNx.sendMessage GojoMdNx.sendMessage(callerId, { text: `*📌 Automatic Block System!*\n👿 Don't Call Me!\nℹ️ Contact me with another whatsapp number and dimand Unblock`}, { quoted : pa7rick })
+    GojoMdNx.sendMessage(callerId, { text: `*📌 Automatic Block System!*\n👿 Don't Call Me!\nℹ️ Contact me with another whatsapp number and dimand Unblock`}, { quoted : pa7rick })
     await sleep(8000)
     await GojoMdNx.updateBlockStatus(callerId, "block")
     }
